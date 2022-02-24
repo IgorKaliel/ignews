83d767b44bc886a94a322b731962964cb7e29acd
Igor Kaliel <igorkaliel@gmail.com>
2022-02-23 21:16:12 -0300

    chore: implement a spread params to receive response on backend

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    
+    console.log(request.query);
+
+    const users = [
+        { id: 1, name: 'Igor' },
+        { id: 1, name: 'Diego' },
+        { id: 1, name: 'Frank' },
+        { id: 1, name: 'Luis' },
+    ]
+
+    return response.json(users);
+
+}
